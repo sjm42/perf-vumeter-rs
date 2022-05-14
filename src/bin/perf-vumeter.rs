992cbaf2d976dd4580d40b96b5f2fdaaaa73f4e8
Sami J. Mäkinen <sjm@iki.fi>
2022-05-15 00:32:25 +0300

    tweaks

--- src/bin/perf-vumeter.rs
+++ src/bin/perf-vumeter.rs
@@ -1,5 +1,6 @@
 // bin/perf-vumeter.rs
 
+use anyhow::bail;
 use log::*;
 use std::fs::{File, OpenOptions};
 use std::io::Write;
@@ -8,7 +9,7 @@ use structopt::StructOpt;
 
 use perf_vumeter::*;
 
-const MAX_DELTA: f64 = 96.0;
+const MAX_DELTA: i16 = 96;
 
 fn main() -> anyhow::Result<()> {
     let opts = OptsCommon::from_args();
@@ -61,13 +62,13 @@ fn main() -> anyhow::Result<()> {
                 .join(" ")
                 .as_str()
         );
-        set_vu(&mut ser, 1, cpu_gauge)?;
+        set_vu(&mut ser, 1, cpu_gauge as i16)?;
 
         // DISK stats + gauge
         let disk_rates = diskstats.diskrates()?;
-        debug!("DISK rates: {disk_rates:?}");
         let disk_gauge = 256.0 * disk_rates[0] / 200_000.0;
-        set_vu(&mut ser, 2, disk_gauge)?;
+        debug!("DISK gauge: {disk_gauge:.1} rates: {disk_rates:?}");
+        set_vu(&mut ser, 2, disk_gauge as i16)?;
 
         // NET stats + gauge
         let rx_rate = rx.bitrate()?;
@@ -75,56 +76,58 @@ fn main() -> anyhow::Result<()> {
         let rate = cmp::max(rx_rate, tx_rate);
         let net_gauge = 256.0 * (((rate as f64) / 1_000_000.0) / (opts.max_mbps as f64));
         debug!(
-            "NET rx: {rx} kbps, tx: {tx} kbps, gauge: {net_gauge}",
+            "NET gauge: {net_gauge:.1} rx: {rx} kbps, tx: {tx} kbps",
             rx = rx_rate / 1000,
             tx = tx_rate / 1000
         );
-        set_vu(&mut ser, 3, net_gauge)?;
+        set_vu(&mut ser, 3, net_gauge as i16)?;
 
         // keep the sample rate from drifting
         elapsed_ns = start.elapsed().as_nanos() as u32;
     }
 }
 
-// only allow values between 0.0 ... 255.0
-fn set_vu(ser: &mut File, channel: u8, mut gauge: f64) -> anyhow::Result<()> {
-    static mut LAST_VAL: [u8; 256] = [0; 256];
+const CHANNELS_NUM: usize = 192; // Remember: channel cmd byte has offset 0x30
+
+fn set_vu(ser: &mut File, channel: u8, mut gauge: i16) -> anyhow::Result<()> {
+    static mut LAST_VAL: [i16; CHANNELS_NUM] = [0; CHANNELS_NUM];
+
     let ch_i = channel as usize;
-    if gauge > 255.0 {
-        gauge = 255.0;
-    }
-    if gauge < 0.0 {
-        gauge = 0.0;
+    if ch_i >= CHANNELS_NUM {
+        bail!(
+            "Channel number too large: {ch_i} (maximum {}",
+            CHANNELS_NUM - 1
+        );
     }
 
-    // do some magic smoothing -- only move the gauge MAX_DELTA at once
-    let delta = unsafe { (LAST_VAL[ch_i] as f64 - gauge).abs() };
-    let delta_pos = unsafe { (gauge - LAST_VAL[ch_i] as f64).is_sign_positive() };
-    let delta_trunc = delta.min(MAX_DELTA) as u8;
-    let value = unsafe {
-        if delta_pos {
-            LAST_VAL[ch_i] + delta_trunc
-        } else {
-            LAST_VAL[ch_i] - delta_trunc
-        }
-    };
+    // limit to gauge values between 0..255
+    if gauge > 255 {
+        gauge = 255;
+    } else if gauge < 0 {
+        gauge = 0;
+    }
+
+    // do some smoothing -- only move the gauge MAX_DELTA at once
+    let delta = unsafe { gauge - LAST_VAL[ch_i] };
+    let delta_sig = delta.signum();
+    let delta_trunc = delta.abs().min(MAX_DELTA);
+    let new_value = unsafe { LAST_VAL[ch_i] + delta_sig * delta_trunc };
     unsafe {
-        LAST_VAL[ch_i] = value;
+        LAST_VAL[ch_i] = new_value;
     }
 
-    let cmd_buf: [u8; 4] = [0xFD, 0x02, 0x30 + channel, value];
-
+    let cmd_buf: [u8; 4] = [0xFD, 0x02, 0x30 + channel, new_value as u8];
     Ok(ser.write_all(&cmd_buf)?)
 }
 
 fn hello(ser: &mut File) -> anyhow::Result<()> {
-    for i in (0..=255u8)
+    for i in (0i16..=255)
         .chain((128..=255).rev())
         .chain(128..=255)
         .chain((0..=255).rev())
     {
-        for c in 1..=3u8 {
-            set_vu(ser, c, i as f64)?;
+        for c in 1u8..=3 {
+            set_vu(ser, c, i)?;
         }
         thread::sleep(time::Duration::new(0, 3_000_000));
     }
